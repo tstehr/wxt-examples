0ebf5615c1689543219e36c2e88b8b33bc35ec0b
Tilman Stehr <tilman@tilman.ninja>
2025-06-13 17:20:00 +0200

    Demonstrate workaround for wxt-dev/wxt#1738
    See wxt-dev/wxt#1738 for more info

--- examples/inject-script/wxt.config.ts
+++ examples/inject-script/wxt.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "wxt";
+import { Plugin } from 'vite';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
@@ -13,4 +14,21 @@ export default defineConfig({
   webExt: {
     startUrls: ["https://wxt.dev"],
   },
+  vite: () => ({
+    plugins: [wrapInIIFE('injected.js')]
+  })
 });
+
+
+function wrapInIIFE(targetFileName: string): Plugin {
+  return {
+    name: 'wrap-injected-in-iife',
+    generateBundle(_, bundle) {
+      for (const [fileName, chunkInfo] of Object.entries(bundle)) {
+        if (fileName === targetFileName && chunkInfo.type === 'chunk' && 'code' in chunkInfo) {
+          chunkInfo.code = `(function(){\n${chunkInfo.code}\n})();`;
+        }
+      }
+    }
+  };
+}
